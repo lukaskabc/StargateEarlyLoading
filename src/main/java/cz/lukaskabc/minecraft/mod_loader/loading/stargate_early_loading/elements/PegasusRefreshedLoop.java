d730dfd09e303f2d60992a48d36723524340bdbb
Lukáš Kaňka <lukaskabc@gmail.com>
2024-12-17 17:44:10 +0100

    add pegasus refreshed loop assets

---
+++ src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/stargate_early_loading/elements/PegasusRefreshedLoop.java
@@ -0,0 +1,13 @@
+package cz.lukaskabc.minecraft.mod_loader.loading.stargate_early_loading.elements;
+
+import net.neoforged.fml.earlydisplay.RenderElement;
+
+import java.util.List;
+
+public class PegasusRefreshedLoop {
+    public static List<RenderElement> create() {
+        return List.of(
+
+        );
+    }
+}
