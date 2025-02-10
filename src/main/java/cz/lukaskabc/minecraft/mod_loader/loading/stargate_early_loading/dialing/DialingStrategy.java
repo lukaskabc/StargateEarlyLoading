16f339b517841c3cbe17cb26fda15aa6e14c5623
Lukáš Kaňka <lukaskabc@gmail.com>
2025-02-11 00:11:53 +0100

    helper methods for DialingStrategy

--- src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/stargate_early_loading/dialing/DialingStrategy.java
+++ src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/stargate_early_loading/dialing/DialingStrategy.java
@@ -6,6 +6,8 @@ import net.neoforged.fml.loading.progress.ProgressMeter;
 import java.util.List;
 
 public abstract class DialingStrategy {
+    public static final String MINECRAFT_PROGRESS = "Minecraft Progress";
+    public static final String EARLY_PROGRESS = "EARLY";
     protected final GenericStargate stargate;
 
     protected DialingStrategy(GenericStargate stargate) {
@@ -14,4 +16,17 @@ public abstract class DialingStrategy {
 
     public abstract void updateProgress(List<ProgressMeter> progressMeters);
 
+    public static float getMinecraftProgress(final List<ProgressMeter> progressMeters) {
+        return progressMeters.stream().filter(meter -> MINECRAFT_PROGRESS.equals(meter.name())).findAny()
+                .orElseGet(DialingStrategy::getNullMeter)
+                .progress();
+    }
+
+    public static boolean isEarlyProgress(final List<ProgressMeter> progressMeters) {
+        return progressMeters.stream().anyMatch(progressMeter -> EARLY_PROGRESS.equals(progressMeter.name()));
+    }
+
+    private static ProgressMeter getNullMeter() {
+        return new ProgressMeter(null, 1, 0, null);
+    }
 }
